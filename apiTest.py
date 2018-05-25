22ecb7ff9cb8d4b06b40d3b3c9b3a6ba5c7f07f2
Devilmoon <luca.giorgi93@gmail.com>
2018-05-25 19:26:56 +0200

    add self feed

--- apiTest.py
+++ apiTest.py
@@ -1,9 +1,11 @@
 from instagram_private_api import Client
 
-user_name = '#########'
-password = '##########'
+user_name = '######'
+password = '#####'
 
 api = Client(user_name, password)
+
+#Get first 10 pics from global feed
 seen = []
 results = api.feed_timeline()
 for i in range(len(results["feed_items"])):
@@ -16,7 +18,27 @@ for i in range(len(results["feed_items"])):
 
 
 print(seen)
+#like first pic seen
+"""
 try:
     api.post_like(seen[0], "feed_timeline")
+except e:
+    print(e)
+"""
+
+#get latest 10 pics from authenticated user
+seenMine=[]
+own = api.self_feed()
+for i in range(len(own["items"])):
+    try:
+        print(own["items"][i]["id"])
+        seenMine.append(own["items"][i]["id"].split("_")[0])
+    except:
+        pass
+
+
+#like latest pic
+try:
+    api.post_like(seenMine[0], "feed_timeline")
 except e:
     print(e)
